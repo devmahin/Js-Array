7e60c8adbc09aefe6a2b5e4d0eecd8ff17c65b5d
devmahin <mahinhowlader123@gmail.com>
2024-01-28 14:46:53 +0600

    idnexOf includefs

--- script.js
+++ script.js
@@ -5,9 +5,66 @@
 // array[2] = 100;
 // console.log(array)
 
-let str = ["Mahin", "Adib", "Hasan", "Raju"]
-    str.push("Add alu")
-    str.pop("Hasan") 
-    str.shift()
-    str.unshift("First add")
-    console.log(str)
+// let str = ["Mahin", "Adib", "Hasan", "Raju"]
+//     str.push("Add alu")
+//     str.pop("Hasan") 
+//     str.shift()
+//     str.unshift("First add")
+//     console.log(str)
+
+
+var foo = [2, 2, 4, 4, 128, 2, 2, 1, 4, 18, 27, 16, 2, 1, 18, 21, 5, 1, 128, 1, 2, 2, 1, 18, 12, 60, 2, 28, 1, 17, 2, 3, 4, 2, 2, 2, 1, 27, 2, 17, 7, 2, 2, 2, 5, 1, 2, 4, 7, 1, 2, 1, 1, 1, 2, 1, 5, 7, 2, 7, 6, 1, 7, 1, 5, 8, 4];
+
+
+
+var fooTopTen = [[4, 128], [18, 128], [25, 60], [27, 28], [10, 27], [37, 27], [15, 21], [9, 18], [14, 18], [23, 18]];
+
+
+
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Grape",
+    "Strawberry",
+    "Blueberry",
+    "Pineapple",
+    "Watermelon",
+    "Mango",
+    "Kiwi",
+    "Peach",
+    "Pear",
+    "Cherry",
+    "Plum",
+    "Raspberry",
+    "Blackberry",
+    "Cranberry",
+    "Lemon",
+    "Lime",
+    "Coconut",
+    "Pomegranate",
+    "Avocado",
+    "Grapefruit",
+    "Apricot",
+    "Fig",
+    "Papaya",
+    "Passion Fruit",
+    "Guava",
+    "Lychee",
+    "Dragon Fruit",
+    "Tangerine",
+    "Cantaloupe",
+    "Honeydew",
+    "Nectarine",
+    "Kumquat",
+    "Star Fruit",
+    "Persimmon",
+    "Clementine",
+    "Gooseberry",
+    "Mulberry",
+    "Mandarin","Pawpaw","Quince","Rambutan","Plantain","Soursop","Ackee","Bilberry","Elderberry","Feijoa"
+];
+console.log(fruits.includes("Soursop"));
+console.log(fruits.indexOf("Feijoa"));
+console.log (Array.isArray(fruits))
+console.log(fruits.join("_"))
